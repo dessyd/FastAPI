229ac0f24c5722a8ac0b89a62e18f7817476a344
Dominique Dessy <dominique.dessy@gmail.com>
2021-11-22 08:45:12 +0100

    From plural to singular function name

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -26,7 +26,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return post
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), 
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), 
                 user_id: int = Depends(oauth2.get_current_user)):
 
     print(user_id)
