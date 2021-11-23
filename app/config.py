759baf0faf57642d4217b2a8c936cae2a5f265ae
Dominique Dessy <dominique.dessy@gmail.com>
2021-11-23 10:17:36 +0100

    Provide default values

--- app/config.py
+++ app/config.py
@@ -1,16 +1,16 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_name: str
+    database_hostname: str = "localhost"
+    database_port: str = "5432"
+    database_name: str = "fastapi"
 
     database_username: str
     database_password: str
 
     secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
 
     class Config:
         env_file = ".env"
