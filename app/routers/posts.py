1a04b4b982625e88807d8b1caf004f76a543ea58
Dominique Dessy <dominique.dessy@gmail.com>
2021-11-24 12:35:14 +0100

    Joins

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -16,21 +16,22 @@ router = APIRouter(
 @router.get("/")
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
 limit: int = 10, skip: int = 0, search: Optional[str] = ""):
-
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     
-    
-    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
         models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
-            models.Post.id).all()
+            models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
-    return results
+    return posts
 
-@router.get("/{id}", response_model=schemas.Post)
+# @router.get("/{id}", response_model=schemas.PostOut)
+@router.get("/{id}")
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
+            models.Post.id).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
