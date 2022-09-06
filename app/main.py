0f4ed1b0c179843448921d9351fbc3317c8840ab
dessyd <reprendre.mezzo_0j@icloud.com>
2022-09-06 09:57:53 +0200

    Adding Meta Data

--- app/main.py
+++ app/main.py
@@ -15,7 +15,60 @@ origins = [
     "http://localhost:8000",
 ]
 
-app = FastAPI()
+description = """
+FastAPI tutorial API helps you do awesome stuff. 🚀
+
+## Posts
+
+You can **read, create, update  and delete posts**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_implemented_).
+* **Read users** (_implemented_).
+
+## Authentication
+
+This is the main login module
+
+## Votes
+
+Here you can cast a vote for a post
+"""
+
+tags_metadata = [
+    {
+        "name": "Users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "Posts",
+        "description": "Manage posts. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Posts external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title="FastAPI tutorial",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Domenico",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dom.do@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata,
+)
 
 app.add_middleware(
     CORSMiddleware,
